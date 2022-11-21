95feff1978d7431e275861ee364ac1df8bc2f066
binarypsilocybin <rogeriosvaldo@gmail.com>
2022-11-21 10:29:52 +0000

    Context API

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import mockUser from './mockData.js/mockUser';
-import mockRepos from './mockData.js/mockRepos';
-import mockFollowers from './mockData.js/mockFollowers';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import mockUser from "./mockData.js/mockUser";
+import mockRepos from "./mockData.js/mockRepos";
+import mockFollowers from "./mockData.js/mockFollowers";
+import axios from "axios";
+import { createContext } from "react";
 
-const rootUrl = 'https://api.github.com';
+const rootUrl = "https://api.github.com";
+
+const GithubContext = React.createContext();
+
+// Provider, Consumer - GithubContext.Provider
+
+const GithubProvider = ({ children }) => {
+  return (
+    <GithubContext.GithubProvider value={"hello"}>
+      {children}
+    </GithubContext.GithubProvider>
+  );
+};
+
+export { GithubProvider, GithubContext };

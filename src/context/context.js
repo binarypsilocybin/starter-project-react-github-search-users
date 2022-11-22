248b0b8051092ecfeacdd811c6ba1805cb7e05fc
binarypsilocybin <rogeriosvaldo@gmail.com>
2022-11-22 12:39:27 +0000

    Mock data

--- src/context/context.js
+++ src/context/context.js
@@ -12,10 +12,13 @@ const GithubContext = React.createContext();
 // Provider, Consumer - GithubContext.Provider
 
 const GithubProvider = ({ children }) => {
+  const [githubUser, setGithubUser] = useState(mockUser);
+  const [repos, setRepos] = useState(mockRepos);
+  const [followers, setFollowers] = useState(mockFollowers);
   return (
-    <GithubContext.GithubProvider value={"hello"}>
+    <GithubContext.Provider value={{ githubUser, repos, followers }}>
       {children}
-    </GithubContext.GithubProvider>
+    </GithubContext.Provider>
   );
 };
 

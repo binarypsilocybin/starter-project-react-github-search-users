b6b3608696843b9f7c60a50ad0c6229de4634077
binarypsilocybin <rogeriosvaldo@gmail.com>
2022-11-27 07:30:15 +0000

    stars and fork

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -40,33 +40,30 @@ const Repos = () => {
 
   // Stars and Forks
 
-  let { stars, forks } = repos.reduce(() => {}, {
-    stars: {},
-    forks: {},
-  });
+  let { stars, forks } = repos.reduce(
+    (total, item) => {
+      const { stargazers_count, name, forks } = item;
+      total.stars[stargazers_count] = { label: name, value: stargazers_count };
+      total.forks[forks] = { label: name, value: forks };
+      return total;
+    },
+    {
+      stars: {},
+      forks: {},
+    }
+  );
+  console.log(stars);
 
-  const chartData = [
-    {
-      label: "HTML",
-      value: "13",
-    },
-    {
-      label: "CSS",
-      value: "23",
-    },
-    {
-      label: "JAVASCRIPT",
-      value: "80",
-    },
-  ];
+  stars = Object.values(stars).slice(-5).reverse();
+  forks = Object.values(forks).slice(-5).reverse();
 
   return (
     <section className="section">
       <Wrapper className="section-center">
         <Pie3D data={mostUsed}></Pie3D>
-        <Column3D data={chartData} />
+        <Column3D data={stars} />
         <Doughnut2D data={mostPopular} />
-        <Bar3D data={chartData} />
+        <Bar3D data={forks} />
       </Wrapper>
     </section>
   );
